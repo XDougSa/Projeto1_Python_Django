e4332db8a10132381d41b1d86b61e184581898ae
Douglas <douglassa321@gmial.com>
2023-09-28 01:07:28 -0300

    OK

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    print('home')
+    return render(
+        request,
+        'home/index.html'
+    )
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Cursos\Curso de Python + Django\Projeto_1\venv
+set VIRTUAL_ENV=C:\Users\dougl\OneDrive\Documentos\Projetos\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
